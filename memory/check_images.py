d053575ae4a08a49ccb8da8302c416529a8be404
Kristian Rother <kristian@spiced-academy.com>
2024-09-24 22:10:47 +0200

    modified all images to 500x500

---
+++ memory/check_images.py
@@ -0,0 +1,18 @@
+"""
+check whether all images in the folder have the target size
+"""
+import os
+
+from PIL import Image
+
+TARGET = (500, 500)
+BLACKLIST = {"probably_fun_memory_0.8.png"}
+
+for fn in os.listdir("."):
+    if (
+        fn.endswith(".png")
+        and fn not in BLACKLIST
+    ):
+        im = Image.open(fn)
+        if im.size != TARGET:
+            print(fn, im.size)
